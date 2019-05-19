e0bacd03e6866de9f3a13615508aa931b65c23eb
Kamil Lesinski <kamilesinski@live.co.uk>
2019-05-19 12:25:17 +0100

    drugi commit

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,6 @@
 package com.sda.algorytmy;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -8,8 +9,52 @@ import java.util.Random;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-       Random random = new Random();
+    public static int[] sort(int[] tab){
+
+        int counter=0;
+        //19
+        for (int j=0; j<tab.length -1; j++) {
+            //19
+            boolean flaga = false;
+            for (int i = 0; i < tab.length -j - 1; i++) {
+
+                if (tab[i] > tab[i + 1]) {
+                    //zamien miejscami
+                    int tmp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = tmp;
+                }
+                counter++;
+            }
+            if (!flaga){
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(tab));
+        System.out.println(counter);
+        return tab;
     }
-}
+    public static void main( String[] args ) throws InterruptedException {
+       Random random = new Random();
+       int max = 5000;
+       int min = -5000;
+        //stworzyc tablice o rozmiarze 10000, ktora bedzie zawierala liczby losowe z zakresu
+        // -5000 do 5000
+        int[] tab = new int[20];
+        long startTime = System.currentTimeMillis();
+        for (int i=0; i<tab.length; i++){
+              int randomNumber = random.nextInt(max - min + 1) + min;
+              tab[i] = randomNumber;
+        }
+        System.out.println(Arrays.toString(tab));
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Czas wykonania: " + totalTime);
+        startTime = System.currentTimeMillis();
+        sort(tab);
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("Czas sortowania: " + totalTime);
+        }
+    }
+
