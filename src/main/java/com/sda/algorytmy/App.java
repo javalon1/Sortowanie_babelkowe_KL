f72fc678d21cb0322baacccd38631150cd8dbd83
Kamil Lesinski <kamilesinski@live.co.uk>
2019-05-19 10:03:02 +0100

    incjalizacja repozytorium

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,15 @@
+package com.sda.algorytmy;
+
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Random random = new Random();
+    }
+}
